b3d960fbad5dab2dbba41ddd769ada28d4cf49d4
Stanislaw <stanislawzhuk@mail.ru>
2016-05-22 01:11:24 +0300

    Add files via upload

---
+++ action.php
@@ -0,0 +1,12 @@
+﻿<!DOCTYPE html> 
+<html>
+<head>
+<title>Ответ</title>
+</head>
+<body>
+<?php
+	echo "<h1>Привет, " . $_POST["fname"] . " " . $_POST["lname"]. "</h1>";
+	echo "<h1>Ваш пол " . $_POST["gender"]. "</h1>";
+?>
+</body>
+</html> 
---
+++ scripts.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+    $('ul > li > a:not(.dont)').click(function() {
+        var url = $(this).attr('href');
+        
+        $.ajax({
+            url: url + '?ajax',
+            success: function(data){
+                $('#content').html(data);
+                history.pushState('', document.title, window.location.pathname);
+                $("#content").css("display", "none");
+                $("#content").fadeIn(500);
+                
+            }
+        });
+        
+        return false;
+    });
+    
+    $("#content").css("display", "none");
+    $("#content").fadeIn(500);
+    
+    $('#scrollup img').mouseover( function(){
+		$( this ).animate({opacity: 0.65},100);
+	}).mouseout( function(){
+		$( this ).animate({opacity: 1},100);
+	}).click( function(){
+		window.scroll(0 ,0); 
+		return false;
+	});
+
+	$(window).scroll(function(){
+		if ( $(document).scrollTop() > 0 )
+			$('#scrollup').fadeIn('fast');
+		else 
+			$('#scrollup').fadeOut('fast');
+	});
+});
